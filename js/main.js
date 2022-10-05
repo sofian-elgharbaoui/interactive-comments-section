87faaefcc10c11cb6d56c17a1c649ec7f8fdc5a0
Soufiane El-Gharbaoui <sofiangharbaoui54@gmail.com>
2022-10-05 18:21:28 +0100

    add new functions and events

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
-let container = document.querySelector(".container");
 import data from "./data.json" assert { type: "json" };
 
+// let commentForm = document.getElementById("create-comment");
+let commentTextarea = document.querySelector("#create-comment textarea");
+let submitButton = document.querySelector("#create-comment button");
+let commentsSection = document.querySelector("#comments");
+let currentUserImg = document.querySelector("#create-comment img");
+currentUserImg.src = data.currentUser.image.png;
+
+let repliesSection = document.createElement("div");
+repliesSection.id = "replies";
+
 for (let i = 0; i < data.comments.length; i++) {
+  let upComment = document.createElement("section");
+  upComment.className = "upComment";
+
   let comment = document.createElement("div");
   comment.className = "comment";
   comment.id = data.comments[i].id;
   comment.innerHTML = `
   <div class="left-side">
     <span>+</span>
-    <span>${data.comments[i].score}</span>
+    <span class="score">${data.comments[i].score}</span>
     <span>-</span>
   </div>
   <div class="right-side">
@@ -17,7 +29,7 @@ for (let i = 0; i < data.comments.length; i++) {
       <h5 class="user-name">${data.comments[i].user.username}</h5>
       <span class="created-at">${data.comments[i].createdAt}</span>
       <span  class="reply-btn">
-        <i class="fa-solid fa-reply"></i> Reply
+      <i class="fa-solid fa-reply"></i> Reply
       </span>
     </div>
     <div class="content">
@@ -25,16 +37,16 @@ for (let i = 0; i < data.comments.length; i++) {
     </div>
   </div>
   `;
-  container.appendChild(comment);
+  upComment.appendChild(comment);
   if (data.comments[i].replies.length > 0) {
-    for (let j = data.comments[i].replies.length - 1; j >= 0; j--) {
+    for (let j = 0; j < data.comments[i].replies.length; j++) {
       let reply = document.createElement("div");
       reply.className = "reply";
       reply.id = data.comments[i].replies[j].id;
       reply.innerHTML = `
           <div class="left-side">
             <span>+</span>
-            <span>${data.comments[i].replies[j].score}</span>
+            <span class="score">${data.comments[i].replies[j].score}</span>
             <span>-</span>
           </div>
           <div class="right-side">
@@ -52,8 +64,57 @@ for (let i = 0; i < data.comments.length; i++) {
             </div>
           </div>
         `;
-
-      comment.after(reply);
+      repliesSection.append(reply);
     }
+    upComment.appendChild(repliesSection);
   }
+  commentsSection.appendChild(upComment);
 }
+
+// I can get elements that have just been created with js, even outside the loop.
+let replyBtn = document.querySelectorAll(".reply-btn");
+for (let i = 0; i < [...replyBtn].length; i++) {
+  const btn = replyBtn[i];
+  btn.onclick = () => {
+    commentTextarea.focus();
+  };
+}
+
+submitButton.onclick = () => {
+  if (commentTextarea.value !== "") {
+    let upComment = document.createElement("section");
+    upComment.className = "upComment";
+
+    let comment = document.createElement("div");
+    comment.className = "comment";
+    comment.innerHTML = `
+    <div class="left-side">
+    <span>+</span>
+    <span class="score">0</span>
+    <span>-</span>
+    </div>
+    <div class="right-side">
+    <div class="user">
+    <img src="${data.currentUser.image.png}" alt="" />
+    <h5 class="user-name">${data.currentUser.username}</h5>
+    <span class="created-at">${
+      new Date().getDate() +
+      " " +
+      new Date().toLocaleString("default", { month: "short" }) +
+      ", " +
+      new Date().getFullYear()
+    }</span>
+    <span class="reply-btn">
+    <i class="fa-solid fa-reply"></i> Reply
+    </span>
+    </div>
+    <div class="content">
+    ${commentTextarea.value}
+    </div>
+    </div>
+    `;
+    upComment.appendChild(comment);
+    commentsSection.appendChild(upComment);
+    commentTextarea.value = "";
+  }
+};
